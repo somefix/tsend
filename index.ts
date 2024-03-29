86912d511dc008728dacb45ae913b29082626f76
Ilya Grigorev <i.grigoryev@elt-poisk.com>
2024-03-29 13:05:58 +0300

    intermediate results

--- index.ts
+++ index.ts
@@ -4,48 +4,54 @@ import pick from "lodash/pick";
 import head from "lodash/head";
 import isNil from "lodash/isNil";
 import every from "lodash/every";
+import findKey from "lodash/findKey";
+import isEmpty from "lodash/isEmpty";
+import isObject from "lodash/isObject";
+import isString from "lodash/isString";
 
-interface IConfigTemplate {
-  [K: string]: { required: string[]; optional?: string[] };
-}
-type TJSendStatuses = "success" | "fail" | "error";
-type ITSendObject = Record<IConfigTemplate[string]["required" | "optional"][number], unknown>
-
-interface IJSendObject {
-  status: TJSendStatuses | string;
-  code?: number;
-  data?: unknown;
-  message?: string;
+enum ETSEndStatuses {
+  SUCCESS = "success",
+  FAIL = "fail",
+  ERROR = "error",
 }
 
-/** Стандартная конфигурация jsend */
-const JSEND_CONFIG: IConfigTemplate = {
+type TConfigTemplate = {
+  [key in ETSEndStatuses]: { required: string[]; optional?: string[] };
+}
+type ITSEndObject<T = unknown> = Record<TConfigTemplate[ETSEndStatuses]["required" | "optional"][number], T>
+
+// interface ITSEndObject<T = unknown> {
+//   status: ETSEndStatuses;
+//   code?: number;
+//   data?: T;
+//   message?: string;
+// }
+
+/**
+ * Стандартная конфигурация jsend
+ * */
+const JSEND_CONFIG: TConfigTemplate = {
   success: { required: ["status", "data"] },
   fail: { required: ["status", "data"] },
   error: { required: ["status", "message"], optional: ["code", "data"] },
 };
 
-// /** Стандартная конфигурация UW 3.0 */
-// const UW_CONFIG: IConfigTemplate = {
-//   success: { required: ["status", "data"] },
-//   fail: { required: ["status", "error"] },
-//   empty: { required: ["status"] },
-// };
-
-/** Проверяем наличие обязательных ключей в объекте */
-function requireKeys<T extends ITSendObject>(
-  keys: IConfigTemplate[string]["required"],
+/**
+ * Проверяем наличие обязательных ключей в объекте
+ * */
+function requireKeys<T extends ITSEndObject>(
+  keys: TConfigTemplate[ETSEndStatuses]["required"],
   data: T
 ): boolean {
   return keys.every((key) => key in data);
 }
 
-/** Проверяем соответствие формата объекту config
+/** Проверяем соответствие формату JSEnd
  *
  * @param {T} data
- * @param {IConfigTemplate} config
+ * @param {TConfigTemplate} config
  * @return {boolean} */
-function isValid<T extends ITSendObject>(
+function isValid<T extends ITSEndObject>(
   data: T,
   config = JSEND_CONFIG
 ): boolean {
@@ -56,79 +62,158 @@ function isValid<T extends ITSendObject>(
   });
 }
 
-/** Проверяем соответствие формата кастомной конфигурации
+/** Проверяем валидность конфигурации
  *
- * @param {T extends IConfigTemplate} config
+ * @param {T extends TConfigTemplate} config
  * @return {boolean} */
-function isValidConfig(config: IConfigTemplate): boolean {
-  return every(config, (specForStatus: IConfigTemplate[number]) => has(specForStatus, 'required'));
+function isValidConfig(config: TConfigTemplate): boolean {
+  return every(config, (specForStatus: TConfigTemplate[ETSEndStatuses]) => has(specForStatus, 'required'));
 }
 
-function generateResponse<T extends ITSendObject>(
+/**
+ * Метод ормирования успешного ответа (success)
+ *
+ * @param { T extends ITSEndObject } data - объект, из которого формируем ответ
+ * @param { TConfigTemplate } config      - объект конфигурации
+ * @param { boolean } forceCreate         - флаг для принудительного формирования
+ * ответа в конфигурации JSEnd
+ * @return { T extends ITSEndObject }
+ * */
+function success<T extends ITSEndObject = ITSEndObject>(
   data: T,
-  config = JSEND_CONFIG
-): T | Error {
-  if (isNil(data) || isNil(data.status)) {
-    return new Error("'data' and 'data.status' must be defined!");
+  config = JSEND_CONFIG,
+  forceCreate = false
+): T {
+  if (!isObject(data)) {
+    return error<T>({ message: '' }, config);
   }
 
-  const { status } = data;
-  const isConfigValid = isValid(data, config);
-  const isJSEndValid = isValid(data);
-  const configByStatus: IConfigTemplate[string] = get(config, status, {
+  if (forceCreate) {
+    const status = ETSEndStatuses.SUCCESS;
+
+    return generateResponse({ status, ...data });
+  }
+
+  return generateResponse(data, config);
+}
+
+/**
+ * Метод ормирования неудачного ответа (fail)
+ *
+ * @param { T extends ITSEndObject } data - объект, из которого формируем ответ
+ * @param { TConfigTemplate } config      - объект конфигурации
+ * @param { boolean } forceCreate         - флаг для принудительного формирования
+ * ответа в конфигурации JSEnd
+ * @return { T extends ITSEndObject }
+ * */
+function fail<T extends ITSEndObject = ITSEndObject>(
+  data: T,
+  config = JSEND_CONFIG,
+  forceCreate = false
+): T {
+  if (!isObject(data)) {
+    return error<T>({ message: '' }, config);
+  }
+
+  if (forceCreate) {
+    const status = ETSEndStatuses.FAIL;
+
+    return generateResponse({ status, ...data });
+  }
+
+  return generateResponse(data, config);
+}
+
+/**
+ * Метод ормирования неудачного ответа (fail)
+ *
+ * @param { T extends ITSEndObject } data - объект, из которого формируем ответ
+ * @param { TConfigTemplate } config      - объект конфигурации
+ * @param { boolean } forceCreate         - флаг для принудительного формирования
+ * ответа в конфигурации JSEnd
+ * @return { T extends ITSEndObject }
+ * */
+function error<T extends ITSEndObject = ITSEndObject>(
+  data: T,
+  config = JSEND_CONFIG,
+  forceCreate = false
+): T {
+  if (!isObject(data)) {
+    return error<T>({ message: '' }, config);
+  }
+
+  if (forceCreate) {
+    const status = ETSEndStatuses.ERROR;
+
+    return generateResponse({ status, ...data });
+  }
+
+  return generateResponse(data, config);
+}
+
+/**
+ * Метод формирования ответа из переданных данных на основе конфигурации
+ *
+ * @param { T extends ITSEndObject } data - объект, из которого формируем ответ
+ * @param { TConfigTemplate } config      - объект конфигурации
+ * @return { T extends ITSEndObject }
+ * */
+function generateResponse<T extends ITSEndObject = ITSEndObject>(
+  data: T,
+  config = JSEND_CONFIG
+): T {
+  if (!isObject(data)) {
+    return error<T>({ message: '' }, config);
+  }
+  // if (isNil(data)) {
+  //   return error<T>({ message: '' }, config);
+  // }
+
+  /** Под статусом подразумевается одно из свойств объекта конфтгурации.
+   * Для конфигурации JSEnd - эти свойства являются статусами. */
+  const status = findKey(config, (spec: TConfigTemplate[ETSEndStatuses]) => requireKeys<T>(spec?.required, data))
+
+  if (isNil(status) || isNil(data)) {
+    /** Такая реализация пока только для конфигурации JSEnd */
+    return error<T>({ message: '' }, config);
+  }
+
+  const configByStatus: TConfigTemplate[ETSEndStatuses] = get(config, status, {
     required: [],
     optional: [],
   });
-  const jsEndConfigByStatus: IConfigTemplate[string] = get(
-    JSEND_CONFIG,
-    status,
-    { required: [], optional: [] }
-  );
-  const required = configByStatus?.required ?? [];
-  const optional = configByStatus?.optional ?? [];
-  const requiredJSEnd = jsEndConfigByStatus?.required ?? [];
-  const optionalJSEnd = jsEndConfigByStatus?.optional ?? [];
-  let resultObject: T = {} as T;
+  const { required = [], optional = [] } = configByStatus;
 
-  if (isConfigValid) {
-    resultObject = pick(data, [...required, ...optional]) as T;
-  } else {
-    if (isJSEndValid) {
-      const dataFieldJSEnd: string = head(
-        requiredJSEnd.filter((key) => key !== "status")
-      ) as string;
-      const dataField: string =
-        head(required.filter((key) => key !== "status")) ?? dataFieldJSEnd;
-
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      resultObject = { [dataField]: data[dataFieldJSEnd], status } as T;
-    } else {
-      resultObject = {
-        ...pick(data, [...requiredJSEnd, ...optionalJSEnd]),
-        status,
-      } as T;
-    }
-  }
-
-  return resultObject;
+  /** Исключаем из входного объекта все поля, кроме обязательных и опциональных из конфигурации */
+  return pick(data, [...required, ...optional]) as T;
 }
 
-export class TSEnd {
-  private readonly _config: IConfigTemplate = JSEND_CONFIG;
+// function success(data: unknown) {
+//   const formattedData: T =
+//     data?.status === status ? data : ({ status, data } as T);
+//
+//   return {
+//     data,
+//     status: 'success'
+//   }
+// }
 
-  constructor(config?: IConfigTemplate) {
+export class TSEnd {
+  private readonly _config: TConfigTemplate = JSEND_CONFIG;
+
+  constructor(config?: TConfigTemplate) {
     if (config && !isValidConfig(config)) {
       throw new Error();
     }
+
     this._config = config ?? this._config;
   }
 
   /**
    * Статический метод валидации.
-   * Проверяем соответствие данных переданному формату или формату JSEnd
+   * Проверяем соответствие данных переданному формату или формату TSEnd
    * @param {T} data
-   * @param {IConfigTemplate?} config
+   * @param {TConfigTemplate?} config
    * @return {boolean} */
   static isValid = isValid;
 
@@ -138,7 +223,7 @@ export class TSEnd {
    *
    * @param {T} data
    * @return {boolean} */
-  public isValid<T extends ITSendObject>(data: T): boolean {
+  public isValid<T extends ITSEndObject>(data: T): boolean {
     return isValid<T>(data, this._config);
   }
 
@@ -146,11 +231,11 @@ export class TSEnd {
    * Метод генерации ответа в соответствии со статусом, указанным в 'data.status'.
    *
    * @param {T} data
-   * @param {IConfigTemplate?} config
+   * @param {TConfigTemplate?} config
    * @return {T | Error} */
-  getResponse<T extends ITSendObject>(
+  getResponse<T extends ITSEndObject>(
     data: T,
-    config?: IConfigTemplate
+    config?: TConfigTemplate
   ): T | Error {
     return generateResponse<T>(data, config ?? this._config);
   }
@@ -159,44 +244,44 @@ export class TSEnd {
    * Метод генерации ответа со статусом 'success'.
    *
    * @param {T} data
-   * @param {IConfigTemplate?} config
-   * @return {T | Error} */
-  static success<T extends ITSendObject>(
-    data: T,
-    config?: IConfigTemplate = JSEND_CONFIG
-  ): T | Error {
-    const status = "success";
-    const formattedData: T =
-      data?.status === status ? data : ({ status, data } as T);
+   * @return {ITSEndObject<T>} */
+  static success<T>(data: T): ITSEndObject<T> {
+    if (isNil(data)) {
+      return TSEnd.error('');
+    }
 
-    return generateResponse<T>(formattedData, config ?? this._config);
+    const status = ETSEndStatuses.SUCCESS;
+
+    return ({ status, data });
   }
 
-  /** Метод генерации ответа со статусом 'fail'.
+  /**
+   * Метод генерации ответа со статусом 'fail'.
    *
    * @param {T} data
-   * @param {IConfigTemplate?} config
-   * @return {T | Error} */
-  fail<T extends ITSendObject>(data: T, config?: IConfigTemplate): T | Error {
-    const status = "fail";
-    const formattedData: T =
-      data?.status === status ? data : ({ status, data } as T);
+   * @return {ITSEndObject<T>} */
+  static fail<T>(data: T): ITSEndObject<T> {
+    if (isNil(data)) {
+      return TSEnd.error('');
+    }
 
-    return generateResponse<T>(formattedData, config ?? this._config);
+    const status = ETSEndStatuses.FAIL;
+
+    return ({ status, data });
   }
 
-  /** Метод генерации ответа со статусом 'error'.
+  /**
+   * Метод генерации ответа со статусом 'error'.
    *
-   * @param {T} data
-   * @param {IConfigTemplate?} config
-   * @return {T | Error} */
-  error<T extends ITSendObject>(data: T, config?: IConfigTemplate): T | Error {
-    const status = "error";
-    const formattedData: T =
-      data?.status === status ? data : ({ status, data } as T);
+   * @param {string} message
+   * @return {ITSEndObject<T>} */
+  static error<T>(message: string): ITSEndObject<T> {
+    if (isEmpty(message) && !isString(message)) {
+      return TSEnd.error('');
+    }
 
-    return generateResponse<T>(formattedData, config ?? this._config);
+    const status = ETSEndStatuses.ERROR;
+
+    return ({ status, message });
   }
 }
-
-export type ITSEnd = TInterface<TSEnd>;
